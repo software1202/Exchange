560f78caf15bac8517c5fe66bf343c04cc7c8dd4
1009495943 <1009495943@qq.com>
2015-01-15 00:19:58 +0800

    liaodong
    2015.15

--- src/Biz/UserBiz.java
+++ src/Biz/UserBiz.java
@@ -1,15 +1,17 @@
 package Biz;
 
+import hibernate.District;
+import hibernate.DistrictDAO;
 import hibernate.HibernateSessionFactory;
 import hibernate.User;
 import hibernate.UserDAO;
 
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Criteria;
-import org.hibernate.Hibernate;
+import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 
 
@@ -33,8 +35,42 @@ public class UserBiz {
 		}else{
 			return null;
 		}
-		
-		
 	}
 	
+	public boolean addUser(String userId,String userPassword,String userName,
+			int integration,String disId,String phoneNum){
+		
+		
+		User user = new User();
+		Date regisDate = new Date(); 
+		//District district = new District();
+		//java.util.Date nowDate = new java.util.Date();
+		//Date regisDate =nowDate.getTime();// new Date()为获取当前系统时间
+		user.setRegisDate(regisDate);
+	    user.setPhoneNumber(phoneNum);
+	    user.setUserId(userId);
+	    user.setUserPassword(userPassword);
+	    user.setUserNickName(userName);
+	    user.setIntegration(integration);
+	    
+	    //district.setDisId(disId);
+	    //userDAO(district.getDisId());
+	    DistrictDAO districtDAO = new DistrictDAO();
+	    District district = districtDAO.findById(disId); 
+	    user.setDistrict(district);
+	    user.setAddress(district.getDisName());
+	    System.out.println(district.getDisName());
+	    System.out.println(user.getDistrict().getDisId());
+	    Session session = HibernateSessionFactory.getSession();
+		session.beginTransaction();
+		session.save(user);
+ 
+		session.getTransaction().commit(); 
+		session.close();
+		return true;
+	}
+	
+	
+	
+
 }
