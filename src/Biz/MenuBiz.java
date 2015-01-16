555667314886f7f2abc548218758060b2cd01c68
1009495943 <1009495943@qq.com>
2015-01-16 10:30:28 +0800

    liaodong
    2015.1.16

--- src/Biz/MenuBiz.java
+++ src/Biz/MenuBiz.java
@@ -11,13 +11,17 @@ public class MenuBiz {
 
 	public List getMenuList(){
 		Session session = HibernateSessionFactory.getSession();
+		session.beginTransaction();
 		Query query = session.createQuery("from Menu");
+		session.close();
 		return query.list();
 	}
 	
 	public List getSubMenuByMenuId(String MenuId){
 		Session session = HibernateSessionFactory.getSession();
+		session.beginTransaction();
 		Query query = session.createQuery("from Submenu where menu='"+MenuId+"'");
+		session.close();
 		return query.list();
 	}
 	
